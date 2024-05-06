19ae2b644b50ec48d49462a9f0b32302e65807f4
Sarhan_Saad <sarhansaad464@gmail.com>
2024-05-06 12:29:37 +0600

    fire base config js file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_SENDERID,
+  appId: import.meta.env.APPID
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
+
